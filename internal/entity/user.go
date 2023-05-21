13533f6b92651500d42901243137bdbee6a3d372
Ádran Carnavale <adran.carnavale@gmail.com>
2023-05-21 18:04:26 -0300

    feat: creates user entity

---
+++ internal/entity/user.go
@@ -0,0 +1,36 @@
+package entity
+
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+
+	"github.com/crnvl96/go-api/pkg/entity"
+)
+
+type User struct {
+	ID       uuid.UUID `json:"id"`
+	Name     string    `json:"name"`
+	Email    string    `json:"email"`
+	Password string    `json:"-"`
+}
+
+func NewUser(name, email, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	user := &User{
+		ID:       entity.NewId(),
+		Name:     name,
+		Email:    email,
+		Password: string(hash),
+	}
+
+	return user, nil
+}
+
+func (user *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	return err == nil
+}
