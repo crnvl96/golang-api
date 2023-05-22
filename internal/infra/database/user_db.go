72afe1fd427ccc224d92f08c6c183f1457352279
Ádran Carnavale <adran.carnavale@gmail.com>
2023-05-22 00:30:25 -0300

    feat: adds user interface with its methods

---
+++ internal/infra/database/user_db.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"gorm.io/gorm"
+
+	"github.com/crnvl96/go-api/internal/entity"
+)
+
+type User struct {
+	DB *gorm.DB
+}
+
+func NewUser(db *gorm.DB) *User {
+	return &User{DB: db}
+}
+
+func (u *User) CreateUser(user *entity.User) error {
+	return u.DB.Create(user).Error
+}
+
+func (u *User) FindByEmail(email string) (*entity.User, error) {
+	var user entity.User
+
+	if err := u.DB.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
