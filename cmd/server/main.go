94464cc0d614c0232f2da9dc7657a3dc99aa02b8
Ádran Carnavale <adran.carnavale@gmail.com>
2023-05-23 11:52:22 -0300

    feat: rehisters new functions of CRUD process

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,40 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 
 	"github.com/crnvl96/go-api/configs"
+	"github.com/crnvl96/go-api/internal/entity"
+	"github.com/crnvl96/go-api/internal/infra/database"
+	"github.com/crnvl96/go-api/internal/infra/webserver/handlers"
 )
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	fmt.Println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+
+	http.ListenAndServe(":8000", r)
 }
